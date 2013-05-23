783a9db85ec8d15ddda767d6fb3d5510c1306287
Zaiste! <oh@zaiste.net>
2013-05-23 16:04:44 +0200

    Clean up and removed unneeded code

--- app.js
+++ app.js
@@ -1,31 +1,12 @@
-// most basic dependencies
 var express = require('express')
   , http = require('http')
   , os = require('os')
   , path = require('path')
   , Faker = require('faker2');
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Expose-Headers', 'Location');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Authorization,x-requested-with,location,content-type');
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
-
-// create the app
 var app = express();
 
-// configure everything, just basic setup
 app.configure(function(){
-  app.use(allowCrossDomain);
   app.set('port', process.env.PORT || 3030);
   app.set('view engine', 'html');
   app.use(express.favicon());
@@ -36,24 +17,18 @@ app.configure(function(){
   app.use(express.static(path.join(__dirname, 'app')));
 });
 
-// simple standard errorhandler
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-//---------------------------------------
-// mini app
-//---------------------------------------
 var openConnections = [];
 
-// simple route to register the clients
-app.get('/stats', function(req, res) {
+app.get('/sse', function(req, res) {
 
     // set timeout as high as possibleg
     req.socket.setTimeout(Infinity);
 
-    // send headers for event-stream connection
-    // see spec for more information
+    // headers for event-stream connection
     res.writeHead(200, {
         'Content-Type': 'text/event-stream',
         'Cache-Control': 'no-cache',
@@ -80,40 +55,32 @@ app.get('/stats', function(req, res) {
     });
 });
 
-setInterval(function() {
+app.post('/widget/:name', function(req, res) {
     // we walk through each connection
     openConnections.forEach(function(resp) {
         var d = new Date();
-        var room = getRandomRoom();
-        console.log(room);
+
+        var widgetName = req.params.name
+        console.log(widgetName);
+
         resp.write('event: ' + 'message' + '\n');
         resp.write('id: ' + d.getMilliseconds() + '\n');
-        resp.write('data:' + createMsg(room) +   '\n\n'); // Note the extra newline
+        resp.write('data:' + createMsg(widgetName) +   '\n\n'); // Note the extra newline
     });
 
-}, 2000);
+    res.send(200);
+});
 
-function createMsg(room) {
+function createMsg(name) {
     msg = {};
-    msg.id = room;
+
+    msg.id = name;
     msg.value = Math.floor((Math.random()*100)+1);
-    msg.updateAt = new Date();
+    msg.updatedAt = new Date();
+
     return JSON.stringify(msg);
 }
 
-function getRandomRoom() {
-  var index = Math.round((Math.random()*2));
-  return ROOMS[index];
-}
-
-function randomDate() {
-  var miliseconds = new Date().getTime() - 100*Math.floor(Math.random()*100000);
-  return new Date(miliseconds);
-}
-
-ROOMS = ['karma', 'valuation', 'progress'];
-
-// startup everything
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 })
