2b013962da5f7e17674dce39869413b45029e689
Zaiste! <oh@zaiste.net>
2013-06-10 00:11:14 +0200

    improved twitter followers widget

--- clock.rb
+++ clock.rb
@@ -8,4 +8,5 @@ module Clockwork
   handler { |worker| Object.const_get(worker).perform_async() }
 
   every(30.seconds, 'TwitterFollowersCount')
+  every(5.seconds, 'Ping')
 end
--- workers/twitter_followers_count.rb
+++ workers/twitter_followers_count.rb
@@ -1,25 +1,25 @@
-require 'net/http'
-require 'json'
-require 'httparty'
+require './settings'
 
 class TwitterFollowersCount
   include Sidekiq::Worker
+  include App::Base
 
   sidekiq_options queue: :kokpit
 
-  HANDLE = 'nukomeet'
+  PARAMS = {
+    handle: 'nukomeet'
+  }
 
   def perform()
-    name = URI::encode(HANDLE)
-
+    name = URI::encode(PARAMS[:handle])
     response = HTTParty.get("http://api.twitter.com/1/users/lookup.json?screen_name=#{name}")
 
     if response.code == 200
       count = JSON.parse(response.body)[0]['followers_count']
-
-      if count
-        HTTParty.post(Settings.endpoint + '/widgets/twitter_followers_count', { value: count })
-      end
+    else
+      count = 0
     end
+
+    self.notify({ value: count })
   end
 end
